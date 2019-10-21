796752f6b8cc88f8f173b5519318287c9d6f7cd2
moira <moira.taylor.2023@student.lhschools.org>
2019-10-21 09:16:23 -0400

    staging all intellij files

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,13 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//todo requirements
+        //must use 2 do/while loops
+        //must use one method public static String guessFeedback(int guess, int answer) {}
+        //must use random number generator and be explicit on #'s to guess from
+        //must have 5 commits or more (method,game loop, logic loop)
+
+    }
+}
